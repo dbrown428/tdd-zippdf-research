2024e608725ec03103da0b3d21b6a8caa174cdfe
D. Brown <dbrown428@gmail.com>
2016-11-22 17:22:47 -0800

    Performance script exports results to a file
    Runs over multiple cases of compression and file quantities.

--- perf.php
+++ perf.php
@@ -1,54 +1,69 @@
 <?php
 
+/**
+ * This script is used to generate some rough PDF zipping duration values
+ * for a research task on a Technical Design Document.
+ */
 printf("Zipping PDFs…\n");
+unlink("./results.csv");
 
-$pdfFiles = [];
-$compression = compressionSetting($argv);
-$totalFileSizeCount = 0;
-$archiveName = "archive.zip";
+$compressionTests = [0, 5, 9];
+$sourceDir = "./source-pdfs";
+$pdfFileFilter = "$sourceDir/*.pdf";
+$files = glob($pdfFileFilter);
 
-if (file_exists($archiveName)) {
-	unlink($archiveName);
-}
+// Determine how many iterations we need. We will increase by 2^i, where 'i' is a 
+// positive integer.
+$fileCount = count($files);
+$maxCount = ceil(log($fileCount, 2)) + 1;
 
-$startTime = microtime(true);
-foreach (glob('./*.pdf') as $filename) {
-	$pdfFiles[] = $filename;
-	$filePath = "./$filename";
-	$totalFileSizeCount += filesize($filePath);
-	
-	exec("zip -q -$compression $archiveName $filePath");
-}
-$endTime = microtime(true);
+// Create and append the first line of the results file.
+appendToResultsFile("File Count, Compression, Duration (s), Average File Size (mb), Archive File Size (mb)");
 
-$count = count($pdfFiles);
-$averageFileSize = bytesToMegabytes($totalFileSizeCount / $count);
-$duration = round($endTime - $startTime, 4);
-$archiveFileSize = bytesToMegabytes(filesize($archiveName));
+// Iterate over all the compression levels we're interested in.
+for ($i = 0; $i < count($compressionTests); $i++) { 
+	$compression = $compressionTests[$i];
 
-// export results as a csv
-// File Count, 
+	// Iterate over all the file counts we're interested in for a decent data set.
+	for ($j = 0; $j < $maxCount; $j++) {
+		$k = pow(2, $j);
 
-printf(" - Zipped $count files\n");
-printf(" - Average file size: ".$averageFileSize."mb\n");
-printf(" - Duration: ".$duration."s\n");
-printf(" - Compression: $compression out of 9\n");
-printf(" - Archive file size: ".$archiveFileSize."mb\n");
+		// Bound to the max file count.
+		if ($k > $fileCount) {
+			$k = $fileCount;
+		}
 
-function compressionSetting($consoleArguments) {
-	$noCompression = 0;
-	$maxCompression = 9;
-	$compression = $consoleArguments[1];
-
-	if ( ! isset($compression)) {
-		return $noCompression;
-	} else if ($compression < $noCompression) {
-		return $noCompression;
-	} else if ($compression > $maxCompression) {
-		return $maxCompression;
-	} else {
-		return $consoleArguments[1];
+		$filesToProcess = array_slice($files, 0, $k);
+		processFiles($filesToProcess, $compression);
 	}
+}
+
+function appendToResultsFile($data) {
+	$appendData = $data.PHP_EOL;
+	file_put_contents("./results.csv", $appendData, FILE_APPEND);
+}
+
+function processFiles($files, $compression) {
+	$archiveName = "./archive.zip";
+	$totalFileSizeCount = 0;	
+
+	if (file_exists($archiveName)) {
+		unlink($archiveName);
+	}
+
+	$startTime = microtime(true);
+	foreach ($files as $filename) {
+		$totalFileSizeCount += filesize($filename);		
+		exec("zip -q -$compression $archiveName $filename");
+	}
+	$endTime = microtime(true);
+
+	$fileCount = count($files);
+	$averageFileSize = bytesToMegabytes($totalFileSizeCount / $fileCount);
+	$duration = round($endTime - $startTime, 4);
+	$archiveFileSize = bytesToMegabytes(filesize($archiveName));
+
+	appendToResultsFile("$fileCount, $compression, $duration, $averageFileSize, $archiveFileSize");
 }
 
 function bytesToMegabytes($bytes) {
